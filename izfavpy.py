83041bf98f207760e92e36311ad123e8071c663b
Irza Pulungan <irzaip@gmail.com>
2017-02-07 10:21:04 +0700

    add file

---
+++ izfavpy.py
@@ -0,0 +1,14 @@
+import os
+import time
+import random
+
+def iz_range(start, end, step):
+  while start <= end:
+    yield start
+    start += step
+        
+        
+        
+def map(value_to_map, begin_val, end_val, begin_mapto, end_mapto):
+  pass
+  
